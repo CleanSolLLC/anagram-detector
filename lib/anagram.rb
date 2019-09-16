8d83664049d3735b4b968f895d0dc73a6422d78b
CleanSolLLC <mamark90@gmail.com>
2019-09-16 10:44:56 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,24 @@
-Class Anagram
+require 'pry'
 
-	def initialize(string)
+class Anagram
 
+	attr_accessor :word_to_match
+
+	def initialize(word_to_match)
+		@word_to_match = word_to_match	
+	end
+
+	def match(array_of_words)
+		anagrams = []
+		
+		array_of_words.each do |word| 
+	
+
+		if word.split("").sort == @word_to_match.split("").sort
+				anagrams << word
+		end
+			 end
+			anagrams
 	end
 
 end
